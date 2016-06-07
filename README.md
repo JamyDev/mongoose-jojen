2f8c8a548238ffedd5fb6ab222e9456b16ff4a53
Jamy Timmermans <me@jamy.be>
2016-06-07 16:53:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# mongoose-jojen
+
+## License
+
+MIT
---
+++ test.js
@@ -0,0 +1,8 @@
+'use strict';
+const expect = require('chai').expect;
+
+const plugin = require('./index.js');
+
+describe('Normal behaviour', () => {
+
+});
